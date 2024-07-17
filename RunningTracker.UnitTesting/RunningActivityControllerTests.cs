6c462f71e63920af52590393f4a5957d07000c45
rbaluyutsnr <133842568+chaaard@users.noreply.github.com>
2024-07-17 23:08:25 +0800

    Update Unit Testing
    Add Running Activity Unit Testing
    
    Fix bugs in User Controller

---
+++ RunningTracker.UnitTesting/RunningActivityControllerTests.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RunningTracker.Application.DTOs;
+using RunningTracker.Application.Interfaces;
+using RunningTracker.Controllers;
+using RunningTracker.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunningTracker.UnitTesting
+{
+    public class RunningActivityControllerTests
+    {
+        private readonly Mock<IRunningActivityService> _mockRunningActivityService;
+        private readonly RunningActivityController _controller;
+
+        public RunningActivityControllerTests()
+        {
+            _mockRunningActivityService = new Mock<IRunningActivityService>();
+            _controller = new RunningActivityController(_mockRunningActivityService.Object);
+        }
+
+        [Fact]
+        public async Task GetAllRunningActivities_ReturnsOk()
+        {
+            var runningActivities = new List<RunningActivity>
+            {
+                new RunningActivity
+                {
+                    Id = 1,
+                    Location = "S&R BGC",
+                    StartDateTime = new DateTime(2024, 7, 15, 6, 0, 0),
+                    EndDateTime = new DateTime(2024, 7, 15, 7, 0, 0),
+                    Distance = 10.0,
+                    UserId = 1,
+                    User = new User { Id = 1, Name = "Richard Baluyut", Weight = 77, Height = 175, BirthDate = new DateTime(1997, 10, 21) }
+                }
+            };
+            _mockRunningActivityService.Setup(service => service.GetAllRunningActivitiesAsync()).ReturnsAsync(runningActivities);
+
+            var result = await _controller.GetAllRunningActivitiesAsync();
+
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var returnedActivities = Assert.IsAssignableFrom<IEnumerable<RunningActivity>>(okResult.Value);
+            Assert.Equal(runningActivities.Count, returnedActivities.Count());
+        }
+
+        [Fact]
+        public async Task GetRunningActivityById_ExistingId_ReturnsOk()
+        {
+            int activityId = 1;
+            var runningActivity = new RunningActivity
+            {
+                Id = activityId,
+                Location = "S&R BGC",
+                StartDateTime = new DateTime(2024, 7, 15, 6, 0, 0),
+                EndDateTime = new DateTime(2024, 7, 15, 7, 0, 0),
+                Distance = 10.0,
+                UserId = 1,
+                User = new User { Id = 1, Name = "Richard Baluyut", Weight = 77, Height = 175, BirthDate = new DateTime(1997, 10, 21) }
+            };
+            _mockRunningActivityService.Setup(service => service.GetRunningActivityByIdAsync(activityId)).ReturnsAsync(runningActivity);
+
+            var result = await _controller.GetRunningActivityById(activityId);
+
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var returnedActivity = Assert.IsType<RunningActivity>(okResult.Value);
+            Assert.Equal(activityId, returnedActivity.Id);
+            Assert.Equal(runningActivity.Location, returnedActivity.Location);
+            Assert.Equal(runningActivity.StartDateTime, returnedActivity.StartDateTime);
+            Assert.Equal(runningActivity.EndDateTime, returnedActivity.EndDateTime);
+            Assert.Equal(runningActivity.Distance, returnedActivity.Distance);
+            Assert.Equal(runningActivity.UserId, returnedActivity.UserId);
+            Assert.Equal(runningActivity.User.Name, returnedActivity.User.Name);
+        }
+
+        [Fact]
+        public async Task CreateRunningActivity_ValidInput_ReturnsCreatedAtAction()
+        {
+            var newRunningActivityDto = new RunningActivityDto
+            {
+                Location = "Landmark BGC",
+                StartDateTime = new DateTime(2024, 7, 16, 6, 0, 0),
+                EndDateTime = new DateTime(2024, 7, 16, 6, 30, 0),
+                Distance = 5.0,
+                UserId = 2
+            };
+            var createdRunningActivity = new RunningActivity
+            {
+                Id = 2,
+                Location = "Landmark BGC",
+                StartDateTime = new DateTime(2024, 7, 16, 6, 0, 0),
+                EndDateTime = new DateTime(2024, 7, 16, 6, 30, 0),
+                Distance = 5.0,
+                UserId = 2,
+                User = new User { Id = 2, Name = "Richard Baluyut", Weight = 77, Height = 175, BirthDate = new DateTime(1997, 10, 21) }
+            };
+            _mockRunningActivityService.Setup(service => service.AddRunningActivityAsync(newRunningActivityDto)).ReturnsAsync(createdRunningActivity);
+
+            var result = await _controller.AddRunningActivity(newRunningActivityDto);
+
+            var actionResult = Assert.IsType<ActionResult<RunningActivity>>(result);
+            var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(actionResult.Result);
+            Assert.Equal("GetRunningActivityById", createdAtActionResult.ActionName);
+            Assert.Equal(createdRunningActivity.Id, createdAtActionResult.RouteValues["id"]);
+        }
+
+        [Fact]
+        public async Task UpdateRunningActivity_ExistingId_ReturnsNoContent()
+        {
+            var updatedRunningActivityDto = new RunningActivityDto
+            {
+                Location = "Uptown Mall BGC",
+                StartDateTime = new DateTime(2024, 7, 15, 6, 0, 0),
+                EndDateTime = new DateTime(2024, 7, 15, 7, 0, 0),
+                Distance = 8.0,
+                UserId = 1
+            };
+            var updatedRunningActivity = new RunningActivity
+            {
+                Id = 1,
+                Location = "Uptown Mall BGC",
+                StartDateTime = new DateTime(2024, 7, 15, 6, 0, 0),
+                EndDateTime = new DateTime(2024, 7, 15, 7, 0, 0),
+                Distance = 8.0,
+                UserId = 1,
+                User = new User { Id = 1, Name = "Richard Baluyut", Weight = 77, Height = 175, BirthDate = new DateTime(1997, 10, 21) }
+            };
+            _mockRunningActivityService.Setup(service => service.UpdateRunningActivityAsync(updatedRunningActivity.Id, updatedRunningActivityDto));
+
+            var result = await _controller.UpdateRunningActivity(updatedRunningActivity.Id, updatedRunningActivityDto);
+
+            Assert.IsType<NoContentResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteRunningActivity_ExistingId_ReturnsNoContent()
+        {
+            int activityId = 1;
+            _mockRunningActivityService.Setup(service => service.DeleteRunningActivityAsync(activityId)).ReturnsAsync(true);
+
+            var result = await _controller.DeleteRunningActivity(activityId);
+
+            Assert.IsType<NoContentResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteRunningActivity_NonExistingId_ReturnsNotFound()
+        {
+            int activityId = 1;
+            _mockRunningActivityService.Setup(service => service.DeleteRunningActivityAsync(activityId)).ReturnsAsync(false);
+
+            var result = await _controller.DeleteRunningActivity(activityId);
+
+            Assert.IsType<NoContentResult>(result);
+        }
+    }
+}
--- RunningTracker.UnitTesting/UserControllerTests.cs
+++ RunningTracker.UnitTesting/UserControllerTests.cs
@@ -4,7 +4,9 @@ using RunningTracker.Application.DTOs;
 using RunningTracker.Application.Interfaces;
 using RunningTracker.Controllers;
 using RunningTracker.Domain.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -26,8 +28,8 @@ namespace RunningTracker.UnitTesting
         {
             var users = new List<User>
             {
-                new User { Name = "Richard Baluyut", Weight = 77, Height = 175, BirthDate = new DateTime(1997, 10, 21) },
-                new User { Name = "Jonel San Diego", Weight = 60, Height = 165, BirthDate = new DateTime(2000, 11, 1) }
+                new User { Id = 1, Name = "Richard Baluyut", Weight = 77, Height = 175, BirthDate = new DateTime(1997, 10, 21) },
+                new User { Id = 2, Name = "Jonel San Diego", Weight = 60, Height = 165, BirthDate = new DateTime(2000, 11, 1) }
             };
 
             _mockUserService.Setup(service => service.GetAllUsersAsync()).ReturnsAsync(users);
@@ -54,6 +56,17 @@ namespace RunningTracker.UnitTesting
         }
 
         [Fact]
+        public async Task GetUserById_NonExistingId_ReturnsNotFound()
+        {
+            int userId = 99;
+            _mockUserService.Setup(service => service.GetUserByIdAsync(userId)).ReturnsAsync((User)null);
+
+            var result = await _controller.GetUserById(userId);
+
+            Assert.IsType<NotFoundResult>(result.Result);
+        }
+
+        [Fact]
         public async Task CreateUser_ValidInput_ReturnsCreatedAtAction()
         {
             var newUserDto = new UserCreateDto { Name = "Richard Baluyut", Weight = 77, Height = 175, BirthDate = new DateTime(1997, 10, 21) };
@@ -65,6 +78,51 @@ namespace RunningTracker.UnitTesting
             var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result);
             Assert.Equal("GetUserById", createdAtActionResult.ActionName);
             Assert.Equal(createdUser.Id, createdAtActionResult.RouteValues["id"]);
+        }
+
+        [Fact]
+        public async Task UpdateUser_ExistingId_ReturnsNoContent()
+        {
+            var updateUserDto = new UserUpdateDto { Name = "Updated Richard Baluyut", Weight = 78, Height = 175, BirthDate = new DateTime(1997, 10, 21) };
+            var updatedUser = new User { Id = 1, Name = "Updated Richard Baluyut", Weight = 78, Height = 175, BirthDate = new DateTime(1997, 10, 21) };
+            _mockUserService.Setup(service => service.UpdateUserAsync(1, updateUserDto)).ReturnsAsync(updatedUser);
+
+            var result = await _controller.UpdateUser(1, updateUserDto);
+
+            Assert.IsType<NoContentResult>(result);
+        }
+
+        [Fact]
+        public async Task UpdateUser_NonExistingId_ReturnsNotFound()
+        {
+            var updateUserDto = new UserUpdateDto { Name = "Non-existing User", Weight = 60, Height = 165, BirthDate = new DateTime(2000, 11, 1) };
+            _mockUserService.Setup(service => service.UpdateUserAsync(99, updateUserDto)).ReturnsAsync((User)null);
+
+            var result = await _controller.UpdateUser(99, updateUserDto);
+
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteUser_ExistingId_ReturnsNoContent()
+        {
+            int userId = 1;
+            _mockUserService.Setup(service => service.DeleteUserAsync(userId)).ReturnsAsync(true);
+
+            var result = await _controller.DeleteUser(userId);
+
+            Assert.IsType<NoContentResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteUser_NonExistingId_ReturnsNotFound()
+        {
+            int userId = 99;
+            _mockUserService.Setup(service => service.DeleteUserAsync(userId)).ReturnsAsync(false);
+
+            var result = await _controller.DeleteUser(userId);
+
+            Assert.IsType<NotFoundResult>(result);
         }
     }
 }
